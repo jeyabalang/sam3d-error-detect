a613706e3df8876efc703bb718857db9c829e847
jeyabalang <jeyabalan360@gmail.com>
2024-06-19 03:50:43 +0200

    Update Real-Time-threshold.py

--- Real-Time-threshold.py
+++ Real-Time-threshold.py
@@ -123,26 +123,20 @@ while True:
   # Add a small delay to simulate real-time data streaming
   time.sleep(1) 
 
-#For instance, if you want to add alerts when certain thresholds are crossed:
-#```python
-# Real-time data update loop
-while True:
-# Get new data 
-    new_data = get_data()
 # Append new data to the existing 
-    dataframe data = pd.concat([data, new_data])
+  dataframe data = pd.concat([data, new_data])
 # Display the updated dataframe
-    data_placeholder.dataframe(data)
+  data_placeholder.dataframe(data)
 # Check for alerts
-    if new_data['temperature'].values[0] > 240:
-        st.error('Temperature is too high!')
-    if new_data['humidity'].values[0] > 45: 
-        st.error('Humidity is too high!')
-    if new_data['vibration'].values[0] > 0.8: 
-        st.error('Vibration levels are too high!')
-    if new_data['pressure'].values[0] > 9: 
-        st.error('Pressure is too high!')
-# Display line charts of the data
+  if new_data['temperature'].values[0] > 240:
+     st.error('Temperature is too high!')
+  if new_data['humidity'].values[0] > 45: 
+     st.error('Humidity is too high!')
+  if new_data['vibration'].values[0] > 0.8: 
+     st.error('Vibration levels are too high!')
+  if new_data['pressure'].values[0] > 9: 
+    st.error('Pressure is too high!')
+    # Display line charts of the data
     st.subheader('Temperature Over Time') 
     chart_placeholder.line_chart(data.set_index('timestamp')['temperature'])
     st.subheader('Humidity Over Time') 
